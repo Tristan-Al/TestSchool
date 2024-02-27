4fff6b5f547a1e74a37dce3d6e8a5a66db43a440
Hugo Jimenez Muñoz <hjimenezdev@gmail.com>
2024-02-27 19:14:39 +0100

    Create controllers

---
+++ app/Http/Controllers/FormationsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Formation;
+
+class FormationsController extends Controller
+{
+
+    // WARNING: I HAD TO ADD A FILLABLE TABLE IN THE TEACHERS MODEL TO BE ABLE TO STORE THE TEACHERS IN THE DATABASE!
+
+    /**
+     * Display a listing of all teachers.
+     *
+     * @return \Illuminate\View\View
+     */
+
+    public function index()
+    {
+        $formations = Formation::all();
+        return view('formations', compact('formations'));
+    }
+
+    /**
+     * Show the form for creating a new teacher.
+     *
+     * @return \Illuminate\View\View
+     */
+
+    public function create()
+    {
+        return view('add.add_formations');
+    }
+
+    /**
+     * Store a newly created teacher in the database.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+
+    public function store(Request $request)
+    {
+        Formation::create([
+            'id' => $request->id,
+            'denomination' => $request->denomination,
+            'acronym' => $request->acronym
+        ]);
+
+        return redirect()->route('formations.index')->with('success', 'Formation agregado correctamente.');
+    }
+
+
+    /**
+     * Shows the edit form for an specific teacher
+     * @param int $id of the teacher
+     * @return view returns a view for the edit form
+     */
+
+    public function edit_formation($id)
+    {
+        //get an specific teacher by its id
+        $formation = DB::table('formations')->where('id', $id)->first();
+        return view('edit.edit_formation', ['formation' => $formation]);
+    }
+
+    /**
+     * Updates the teacher from the edit form
+     * @param int $id of the teacher
+     * @param Request 
+     * @return view returns a view of the list of teachers
+     */
+
+    public function update_formation(Request $request, $id)
+    {
+
+        $formation = Formation::findOrFail($id);
+
+        $formation->update([
+            'id' => $request->input('id'),
+            'denomination' => $request->input('denomination'),
+            'acronym' => $request->input('acronym')
+        ]);
+
+        return redirect()->route('formations.index')->with('success', 'Formation updated successfully');
+
+    }
+
+    public function delete_formation($id)
+    {
+        $formation = Formation::findOrFail($id);
+        $formation->delete();
+        return redirect()->route('formations.index')->with('success', 'Formation deleted successfully');
+    }
+
+}
