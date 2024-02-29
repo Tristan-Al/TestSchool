aa5a4bd59c7e625d040b33071c6ee38187c6b73c
Fortuna-457 <alejandro.castrocanalejo@gmail.com>
2024-02-29 16:59:17 +0100

    Modified navigation.blade.php, based on our design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,20 +12,13 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="{{ asset('table.css') }}" rel="stylesheet" />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <header class="bg-white dark:bg-gray-800 shadow">
+                @include('layouts.navigation')
+            </header>
 
             <!-- Page Content -->
             <main>
