8080ee3035b9c25fd84c6065dc5da43645429fe5
Hugo Jimenez Muñoz <hjimenezdev@gmail.com>
2024-03-01 20:27:42 +0100

    Modify logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,6 +43,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect()->route('dashboard');
+        return redirect()->route('home');
     }
 }
