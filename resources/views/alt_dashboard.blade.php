ec026a9148faeadc5dfdd3a4bfae90212c59c795
ManuelMarAco <martinacostamanuel.tramites@gmail.com>
2024-03-05 11:59:21 +0100

    Stylesheet added

--- resources/views/alt_dashboard.blade.php
+++ resources/views/alt_dashboard.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
         
         <link href="{{ asset('table.css') }}" rel="stylesheet" />
     </head>
